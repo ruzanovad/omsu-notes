984d2f889a6033921a706e87222dc50ebb6efe25
ruzanovad <kaiser7lu@gmail.com>
2023-04-23 14:25:56 +0600

    lectures

--- prob/Conditional_expectation.py
+++ prob/Conditional_expectation.py
@@ -1,27 +1,36 @@
 from fractions import *
 
-# Условное мат.ожидание
-xi = [-1, 0, 1]
-eta = [-1, 0, 1]
-# table = [[Fraction(1, 8), Fraction(1, 6), Fraction(5, 12)],
-#          [Fraction(0), Fraction(1, 12), Fraction(7, 48)],
-#          [Fraction(0), Fraction(0), Fraction(1, 16)]]
-table = [[Fraction(1, 8), Fraction(1, 12), Fraction(7, 24)],
-         [Fraction(2, 24), Fraction(1, 12), Fraction(1, 16)],
-         [Fraction(3, 24), Fraction(1, 12), Fraction(1, 16)]]
-for i in range(3):
-    g = Fraction(0)
-    M = 0
-    for j in range(3):
-        g+=table[i][j]
-        M+=xi[j]*table[i][j]
 
-    # for j in range(3):
-    #     g+=table[j][i]
-    #     M+=xi[j]*table[j][i]
+with open("input.txt") as file:
+    # Условное мат.ожидание
+    xi = [Fraction(i) for i in file.readline().split()]
+    eta = [Fraction(i) for i in file.readline().split()]
 
-    print(g)
+    # table = [[Fraction(1, 8), Fraction(1, 6), Fraction(5, 12)],
+    #          [Fraction(0), Fraction(1, 12), Fraction(7, 48)],
+    #          [Fraction(0), Fraction(0), Fraction(1, 16)]]
+    table = []
+    for i in range(len(xi)):
+        # for j in range(len(eta)):
+        table.append([Fraction(i) for i in file.readline().split()])
 
-    print(M)
-    print(M/g)
-    print("---")
+    # table = [[Fraction(1, 8), Fraction(1, 12), Fraction(7, 24)],
+    #         [Fraction(2, 24), Fraction(1, 12), Fraction(1, 16)],
+    #         [Fraction(3, 24), Fraction(1, 12), Fraction(1, 16)]]
+    for i in range(3):
+        g = Fraction(0)
+        M = 0
+        for j in range(len(xi)):
+            g+=table[i][j]
+            M+=xi[j]*table[i][j]
+
+        # for j in range(len(eta)):
+        #     g+=table[j][i]
+        #     M+=eta[j]*table[j][i]
+
+        # print(g)
+
+        # print(M)
+        print(M/g)
+        print("---")
+
