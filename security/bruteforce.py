9d056de459ef05cbf8ae81f3cd6335f2c8da3cf3
ruzanovad <kaiser7lu@gmail.com>
2023-05-16 10:21:28 +0600

    automata

--- security/bruteforce.py
+++ security/bruteforce.py
@@ -12,12 +12,12 @@
 
 cnt  = 0
 
-for i in range(26):
+for i in range(1, 26):
     for j in range(26):
-        for k in range(26):
+        for k in range(1, 26):
             for f in range(26):
                 # i j
                 # k f
-                if (i % 13 or k % 13) and ((j * 13) % 26 == i and (f * 13) % 26 == k):
+                if (i % 13 or k % 13) and ((j % i)%13==0 and (f ) % k %13 == 0):
                     cnt+=1
 print(cnt)
