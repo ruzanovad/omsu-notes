8e7b976dad721f1631806add47ec850b2785aed7
ruzanovad <kaiser7lu@gmail.com>
2023-12-30 23:43:11 +0600

    MT lecture №1

--- 3thgrade/kursovaya/3-coloring_algo.py
+++ 3thgrade/kursovaya/3-coloring_algo.py
@@ -71,16 +71,26 @@ def generate_matrices_helper(matrix, row, matrices):
         matrix[row][col] = 0
 
 
+def return_str_repr(lst):
+    s = []
+    for i in lst:
+        s.append(''.join(map(str, i)))
+    return '\n'.join(s)
 # Example usage:
-for n in range(5, 6):
+for n in range(8, 9):
     count = 0
     adjacency_matrices = generate_adjacency_matrices(n)
+    lst = set(list (map(return_str_repr, adjacency_matrices)))
+    assert len(lst) == 2**(n*(n-1)/2)
+
     # igraph.Graph.to_undirected()
     dictionary = {}
+    edgelist_dict = {}
     for index, matrix in enumerate(adjacency_matrices):
         # print(*[' '.join(map(str, i)) for i in matrix])
         graph = igraph.Graph.Adjacency(matrix)
         graph.to_undirected()
+
         # igraph.plot(graph)
 
         # fig, axs = plt.subplots(64, 64, figsize=(64, 53))
@@ -88,26 +98,46 @@ for n in range(5, 6):
         # axs[1].bar(x=[0, 1, 2], height=[1, 5, 3], color='tomato')
         # igraph.Graph.get_edgelist().__len__()
         # print(graph.)
-        l = graph.cliques(min=4, max=4)
-        if len(l) > 0:
-            # print(*l)
-            count += 1
-            if graph.get_edgelist().__len__() not in dictionary.keys():
-                dictionary[graph.get_edgelist().__len__()] = [graph]
-            else:
-                flag = False
-                for grp in dictionary[graph.get_edgelist().__len__()]:
-                    flag = graph.isomorphic(grp)
-                    if flag:
-                        break
-                if not flag:
-                    dictionary[graph.get_edgelist().__len__()].append(graph)
-            # fig, ax = plt.subplots()
+        components = graph.decompose()
+        for i in components:
+            if len(i.vs) == 4:
+                l = graph.cliques(min=4, max=4)
+                if len(l) > 0:
+                    count+=1
+                    if (graph.get_edgelist().__len__() in edgelist_dict.keys()):
+                        edgelist_dict[graph.get_edgelist().__len__()]+=1
+                    else :
+                        edgelist_dict[graph.get_edgelist().__len__()] = 1
+                    igraph.plot(graph, layout='circle', target='%d_%d_cli_%d.png' % (n, graph.get_edgelist().__len__(), index))
 
+                    break
+        # l = graph.cliques(min=4, max=4)
+        # if len(l) > 0:
+        #     # print(*l)
+        #     count += 1
+        #     if (graph.get_edgelist().__len__() in edgelist_dict.keys()):
+        #         edgelist_dict[graph.get_edgelist().__len__()]+=1
+        #     else :
+        #         edgelist_dict[graph.get_edgelist().__len__()] = 1
+        #     # if graph.get_edgelist().__len__() not in dictionary.keys():
+        #     #     dictionary[graph.get_edgelist().__len__()] = [graph]
+        #     # else:
+        #     #     flag = False
+        #     #     for grp in dictionary[graph.get_edgelist().__len__()]:
+        #     #         flag = graph.isomorphic(grp)
+        #     #         if flag:
+        #     #             break
+        #     #     if not flag:
+        #     #         dictionary[graph.get_edgelist().__len__()].append(graph)
+        #     # fig, ax = plt.subplots()
+        #     igraph.plot(graph, layout='circle', target='%d_clique_%d.png' % (graph.get_edgelist().__len__(), index))
+        #
             # plt.show()
             # print(matrix)
-    for val in dictionary.values():
-        for index, value in enumerate(val):
-            igraph.plot(value, layout='circle', target='%d_clique_%d.png' % (value.get_edgelist().__len__(), index))
+    # for val in dictionary.values():
+    #     for index, value in enumerate(val):
+    #         igraph.plot(value, layout='circle', target='%d_clique_%d.png' % (value.get_edgelist().__len__(), index))
     print(count, 2 ** (n * (n - 1) // 2), count / 2 ** (n * (n - 1) // 2),
           fractions.Fraction(count, 2 ** (n * (n - 1) // 2)))
+    print(edgelist_dict)
+    
