6f7885b38e5dec2204b767bd9018e42e4c7983b7
ruzanovad <kaiser7lu@gmail.com>
2024-03-18 10:09:19 +0600

    numeric methods 1 ruining the input matrix got rid of randomizer

--- 3thgrade/numeric_methods/main.cpp
+++ 3thgrade/numeric_methods/main.cpp
@@ -4,9 +4,8 @@
 #include <numeric>
 #include <cmath>
 #include <cassert>
-#include <random>
 #include <iomanip>
-
+#include <ctime>
 
 #define matrix vector<vector<double>>
 
@@ -72,24 +71,32 @@ matrix multmat(const matrix &A, const matrix &B) {
     return result;
 }
 
-int get_sign() {
-    int sign = rand() & 1;
-    if (sign == 0) {
-        sign = -1;
+double get_sign(int rule, int i) {
+    double sign;
+    switch (rule) {
+        case 1:
+            sign = 2. * (i % 2) - 1.;
+            break;
+        case 2:
+            sign = 2. * ((i + 1) % 2) - 1.;
+            break;
+        default:
+            sign = 1.;
     }
     return sign;
 }
 
-vector<double> LU(const matrix &A, const vector<double> &b) {
+// копии матрицы в тестирующей части
+vector<double> LU(matrix &U, vector<double> &b) {
     matrix L;     // L - нулевая изначально
-    vector<int> P;
+//    vector<int> P;
 
-    size_t n = A.size();
+    size_t n = U.size();
     L.assign(n, vector<double>(n, 0));
-    P.resize(n);
-    iota(P.begin(), P.end(), 0);
+//    P.resize(n);
+//    iota(P.begin(), P.end(), 0);
 
-    auto U = A;
+//    auto U = A;
 
     for (int i = 0; i < n; ++i) {
         double maxElement = abs(U[i][i]);
@@ -101,9 +108,11 @@ vector<double> LU(const matrix &A, const vector<double> &b) {
                 pivotRow = k;
             }
         }
+        // просто переставляем строчки
         swap(U[i], U[pivotRow]);
-        swap(P[i], P[pivotRow]);
+//        swap(P[i], P[pivotRow]);
         swap(L[i], L[pivotRow]);
+        swap(b[i], b[pivotRow]);
 
         // Decomposition
         for (int j = i + 1; j < n; ++j) {
@@ -112,18 +121,22 @@ vector<double> LU(const matrix &A, const vector<double> &b) {
                 U[j][k] -= L[j][i] * U[i][k];
             }
         }
+        L[i][i] = 1;
     }
+//    printMatrix(L);
+//    printMatrix(U);
+//    printVector(b);
+//    vector<double> Pb(b.size());
+//    for (int i = 0; i < U.size(); ++i) {
+//        Pb[i] = b[P[i]];
+//    }
 
-    vector<double> Pb(b.size());
-    for (int i = 0; i < U.size(); ++i) {
-        Pb[i] = b[P[i]];
-    }
-    vector<double> x = backwardSubstitution(U, forwardSubstitution(L, Pb));
+    vector<double> x = backwardSubstitution(U, forwardSubstitution(L, b));
     return x;
 }
 
 vector<double> forwardSubstitution(const matrix &L, const vector<double> &Pb) {
-    size_t n = L.size();
+    size_t n = L.size(); // L[i][i] is 1
     vector<double> y(n, 0.0);
     for (int i = 0; i < n; ++i) {
         double sum = 0.0;
@@ -174,7 +187,8 @@ void print_results(matrix &A, vector<double> &b, const vector<double> &expected_
         cout << "Правая часть:" << endl;
         printVector(b);
     }
-
+    auto A_copy(A);
+    auto b_copy(b);
     vector<double> actual_x = LU(A, b);
 
     cout << "Точное решение:" << endl;
@@ -186,50 +200,44 @@ void print_results(matrix &A, vector<double> &b, const vector<double> &expected_
     cout << "Ошибка:" << norm(z) << endl;
     cout << "Относительная ошибка: " << norm(z) / norm(expected_x) << endl;
 
-    auto mul = mult(A, actual_x);
-    auto r = subtract(mul, b);
+    auto mul = mult(A_copy, actual_x);
+    auto r = subtract(mul, b_copy);
 
     cout << "Невязка: " << norm(r) << endl;
 
-    cout << "Относительная невязка: " << norm(r) / norm(b) << endl;
+    cout << "Относительная невязка: " << norm(r) / norm(b_copy) << endl;
 
     assert(norm(z) < eps);
     cout << endl;
 }
 
-vector<double> generate_actual_solution(int n) {
-    std::random_device r;
-
-    std::default_random_engine e1(r());
-    std::uniform_int_distribution<int> uniform_dist(-100, 100);
-
+vector<double> generate_actual_solution(int n, double alpha = 1, double beta = 1) {
     vector<double> return_value(n, 0);
-    for (auto &x: return_value) {
-        x = uniform_dist(e1);
+    for (int i = 0; i < n; ++i) {
+        double pi_half = acos(-1.) * 0.5;
+        double gamma = sin(pi_half * ((i + 0.0) / (n - 1)));
+        double sign = get_sign(2, i);
+        return_value[i] = 1. / (sign * ((1 - gamma) * alpha + gamma * beta));
     }
     return return_value;
 }
 
-matrix first_class(int n, double alpha, double beta, bool inv = false) {
+matrix first_class(int n, double alpha, double beta, bool inv = false, int sign_rule = 1) {
     matrix T(n, vector<double>(n, 0)), T_inv(n, vector<double>(n, 0)),
             J(n, vector<double>(n, 0)), return_value(n, vector<double>(n, 0));
     T[0][0] = 1;
     T_inv[0][0] = n;
 
     double gamma = 0;
-    int sign = get_sign();
+    double sign = get_sign(sign_rule, 0);
     if (inv) {
         J[0][0] = 1. / (sign * ((1 - gamma) * alpha + gamma * beta));
         for (int i = 1; i < n; ++i) {
             T[i][i] = 2;
-            T[i - 1][i] = -1;
-            T[i][i - 1] = -1;
+            T[i - 1][i] = T[i][i - 1] = -1;
+            T_inv[i][i] = T_inv[0][i] = T_inv[i][0] = n - i;
 
-            T_inv[i][i] = n - i;
-            T_inv[0][i] = n - i;
-            T_inv[i][0] = n - i;
-
-            sign = get_sign();
+            sign = get_sign(sign_rule, i);
             gamma = sqrt((i + 0.) / (n - 1));
             J[i][i] = 1. / (sign * ((1 - gamma) * alpha + gamma * beta));
         }
@@ -237,51 +245,37 @@ matrix first_class(int n, double alpha, double beta, bool inv = false) {
         J[0][0] = sign * ((1 - gamma) * alpha + gamma * beta);
         for (int i = 1; i < n; ++i) {
             T[i][i] = 2;
-            T[i - 1][i] = -1;
-            T[i][i - 1] = -1;
+            T[i - 1][i] = T[i][i - 1] = -1;
 
-            T_inv[i][i] = n - i;
-            T_inv[0][i] = n - i;
-            T_inv[i][0] = n - i;
+            T_inv[i][i] = T_inv[0][i] = T_inv[i][0] = n - i;
 
-            sign = get_sign();
+            sign = get_sign(sign_rule, i);
             gamma = sqrt((i + 0.) / (n - 1));
             auto value = sign * ((1 - gamma) * alpha + gamma * beta);
             J[i][i] = value;
         }
-//        printMatrix(J);
-//        cout << endl;
-
     }
     return_value = multmat(multmat(T, J), T_inv);
-//    printMatrix(T);
-//    cout << endl;
-//    printMatrix(T_inv);
-//    cout << endl;
-//    printMatrix(J);
+
     return return_value;
 }
 
-matrix second_class(int n, double alpha, double beta, bool inv = false) {
+matrix second_class(int n, double alpha, double beta, bool inv = false, int sign_rule = 1) {
     matrix T(n, vector<double>(n, 0)), T_inv(n, vector<double>(n, 0)),
             J(n, vector<double>(n, 0)), return_value(n, vector<double>(n, 0));
     T[0][0] = 1;
     T_inv[0][0] = n;
 
     double gamma = 0;
-    int sign = get_sign();
+    double sign = get_sign(sign_rule, 0);
     if (!inv) {
         J[0][0] = sign * ((1 - gamma) * alpha + gamma * beta);
         for (int i = 1; i < n; ++i) {
-            T[i][i] = 2;
-            T[i - 1][i] = -1;
-            T[i][i - 1] = -1;
+            T[i - 1][i] = T[i][i - 1] = -1;
 
-            T_inv[i][i] = n - i;
-            T_inv[0][i] = n - i;
-            T_inv[i][0] = n - i;
+            T_inv[i][i] = T_inv[0][i] = T_inv[i][0] = n - i;
 
-            sign = get_sign();
+            sign = get_sign(sign_rule, i);
             gamma = sqrt((i + 0.) / (n - 1));
             J[i][i] = sign * ((1 - gamma) * alpha + gamma * beta);
         }
@@ -293,15 +287,11 @@ matrix second_class(int n, double alpha, double beta, bool inv = false) {
         J[0][1] = -J[0][0] * J[0][0]; // клетка 2 на 2
 
         for (int i = 1; i < n; ++i) {
-            T[i][i] = 2;
-            T[i - 1][i] = -1;
-            T[i][i - 1] = -1;
+            T[i - 1][i] = T[i][i - 1] = -1;
 
-            T_inv[i][i] = n - i;
-            T_inv[0][i] = n - i;
-            T_inv[i][0] = n - i;
+            T_inv[i][i] = T_inv[0][i] = T_inv[i][0] = n - i;
 
-            sign = get_sign();
+            sign = get_sign(sign_rule, i);
             gamma = sqrt((i + 0.) / (n - 1));
             J[i][i] = 1. / (sign * ((1 - gamma) * alpha + gamma * beta));
         }
@@ -318,8 +308,8 @@ int main() {
                 {-4, -2, 8}
         };
         vector<double> b = {-8, -11, -3};
-        vector<double> expected_x = {-24.625, -127./12, -46./3};
-        vector<double> actual_x = LU(A, b);
+        vector<double> expected_x = {-24.625, -127. / 12, -46. / 3};
+//        vector<double> actual_x = LU(A, b);
 
         print_results(A, b, expected_x);
     }
@@ -331,7 +321,7 @@ int main() {
         };
         vector<double> b = {2, 1, 1};
         vector<double> expected_x = {-1, 1, 1};
-        vector<double> actual_x = LU(A, b);
+//        vector<double> actual_x = LU(A, b);
 
         print_results(A, b, expected_x);
     }
@@ -412,28 +402,25 @@ int main() {
     for (auto &pair: params) {
         auto alph = pair.first, beta = pair.second;
 
-        auto A = first_class(n, alph, beta);
+        auto A = first_class(n, alph, beta), A_copy(A);
         auto A_inv = first_class(n, alph, beta, true);
 
 //        printMatrix(A_inv);
 
 //        cout << endl;
-        auto x_starred = generate_actual_solution(n);
+        auto x_starred = generate_actual_solution(n, alph, beta);
 
-//        printVector(x_starred);
-//        cout << endl;
-//        cout << endl;
-
-        auto f = mult(A, x_starred);
+        auto f = mult(A, x_starred), f_copy(f);
 
         auto x_tilde = LU(A, f);
 
-        vector<double> r = subtract(mult(A, x_tilde), f);
+        vector<double> r = subtract(mult(A_copy, x_tilde), f_copy);
 
-        double error = norm(subtract(x_tilde, x_starred)), zeta = error / norm(x_starred), rho = norm(r) / norm(f);
+        double error = norm(subtract(x_tilde, x_starred)), zeta = error / norm(x_starred), rho = norm(r) / norm(f_copy);
 
-        cout << setprecision(3) << alph << '\t' << beta << '\t' << norm(A) << '\t' << norm(A_inv) << '\t' << norm(A) *
-                                                                                                             norm(A_inv)
+        cout << setprecision(3) << alph << '\t' << beta << '\t' << norm(A_copy) << '\t' << norm(A_inv) << '\t'
+             << norm(A_copy) *
+                norm(A_inv)
              << '\t' << error
              << '\t' << zeta << '\t' << norm(r) << '\t' << rho << endl;
     }
@@ -443,28 +430,22 @@ int main() {
     for (auto &pair: params) {
         auto alph = pair.first, beta = pair.second;
 
-        auto A = second_class(n, alph, beta);
+        auto A = second_class(n, alph, beta), A_copy(A);
         auto A_inv = second_class(n, alph, beta, true);
 
-//        printMatrix(A_inv);
+        auto x_starred = generate_actual_solution(n, alph, beta);
 
-//        cout << endl;
-        auto x_starred = generate_actual_solution(n);
-
-//        printVector(x_starred);
-//        cout << endl;
-//        cout << endl;
-
-        auto f = mult(A, x_starred);
+        auto f = mult(A, x_starred), f_copy(f);
 
         auto x_tilde = LU(A, f);
 
-        vector<double> r = subtract(mult(A, x_tilde), f);
+        vector<double> r = subtract(mult(A_copy, x_tilde), f_copy);
 
-        double error = norm(subtract(x_tilde, x_starred)), zeta = error / norm(x_starred), rho = norm(r) / norm(f);
+        double error = norm(subtract(x_tilde, x_starred)), zeta = error / norm(x_starred), rho = norm(r) / norm(f_copy);
 
-        cout << setprecision(3) << alph << '\t' << beta << '\t' << norm(A) << '\t' << norm(A_inv) << '\t' << norm(A) *
-                                                                                                             norm(A_inv)
+        cout << setprecision(3) << alph << '\t' << beta << '\t' << norm(A_copy) << '\t' << norm(A_inv) << '\t'
+             << norm(A_copy) *
+                norm(A_inv)
              << '\t' << error
              << '\t' << zeta << '\t' << norm(r) << '\t' << rho << endl;
     }
