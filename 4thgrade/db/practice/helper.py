86edf5284cb761ea698c289878c7ad5f4bedb382
Daria Ruzanova <kaiser7lu@gmail.com>
2024-11-09 16:49:27 +0600

    fix: added some functions

--- 4thgrade/db/practice/helper.py
+++ 4thgrade/db/practice/helper.py
@@ -7,7 +7,7 @@ def decompose_fd(fds):
     на несколько зависимостей с одним атрибутом справа
     """
     decomposed_fds = []
-    for left, right in fds.items():
+    for left, right in fds:
         for r in right:
             decomposed_fds.append((left, {r}))
     return decomposed_fds
@@ -38,6 +38,8 @@ def remove_redundant_fds(fds):
         closure = find_closure(fd[0], minimized_fds)
         if not fd[1].issubset(closure):
             minimized_fds.append(fd)
+        else:
+            print("Удаляем ФЗ", fd[0], "->", fd[1])
     return minimized_fds
 
 
@@ -46,15 +48,21 @@ def remove_redundant_attributes(fds):
     Удаляет избыточные атрибуты из левой части каждой зависимости
     """
     minimized_fds = []
-    for left, right in fds:
-        left = list(left)
-        for attr in left:
-            test_left = set(left)
+    for left, rights in fds:
+        left = set(left)  # Копируем левую часть для модификации
+        for attr in list(left):  # Итерируемся по копии списка атрибутов
+            test_left = left.copy()
             test_left.remove(attr)
-            closure = find_closure(test_left, fds)
-            if right.issubset(closure):
+            # Формируем новое множество зависимостей G
+            G = [fd for fd in fds if fd != (left, rights)]
+            G.append((test_left, rights))
+            closure = find_closure(test_left, G)
+            if rights.issubset(closure):
                 left.remove(attr)
-        minimized_fds.append((set(left), right))
+                print(
+                    f"Удаляем избыточный атрибут '{attr}' из зависимости: {left | {attr}} -> {rights}"
+                )
+        minimized_fds.append((left, rights))
     return minimized_fds
 
 
@@ -62,20 +70,33 @@ def minimal_cover(fds):
     """
     Строит минимальное покрытие для множества функциональных зависимостей
     """
-    decomposed_fds = decompose_fd(fds)
-    without_redundant_fds = remove_redundant_fds(decomposed_fds)
+    # decomposed_fds = decompose_fd(fds)
+    without_redundant_fds = remove_redundant_fds(fds)
     minimized_fds = remove_redundant_attributes(without_redundant_fds)
     return minimized_fds
 
 
+def reading_function(file="decomposed-deps.md"):
+    fds = []
+    with open(file, "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            left, right = line.strip().split(" -> ")
+            left_set = set(left.split(", "))  # Преобразуем левую часть в множество
+            right_set = set(right.split(", "))  # Преобразуем правую часть в множество
+            fds.append((left_set, right_set))
+    return fds
+
+
 # Пример использования
-fds = {
-    ("A1",): {"A2", "A3", "A4", "A5", "A6", "A7", "A8"},
-    ("B1",): {"B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10", "B11"},
-    ("C1",): {"C2"},
-    ("D1",): {"D2"},
+# fds = {
+#     ("A1",): {"A2", "A3", "A4", "A5", "A6", "A7", "A8"},
+#     ("B1",): {"B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10", "B11"},
+#     ("C1",): {"C2"},
+#     ("D1",): {"D2"},
+# }
 
-}
-
+fds = reading_function()
+print(fds)
 min_cover = minimal_cover(fds)
 print("Минимальное покрытие:", min_cover)
