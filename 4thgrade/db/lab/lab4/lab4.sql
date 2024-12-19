4cfa53f9e02bbdf7a5cdaa08d3014b9bfeff9aad
Daria Ruzanova <kaiser7lu@gmail.com>
2024-12-19 17:17:58 +0600

    the remaining are 14 15

--- 4thgrade/db/lab/lab4/lab4.sql
+++ 4thgrade/db/lab/lab4/lab4.sql
@@ -48,6 +48,14 @@ VALUES
 (3, 400, '2023-12-25', 'завершен'),
 -- Заказы за 2024 год
 (1, 150, '2024-01-15', 'завершен'),
+(1, 150, '2024-02-15', 'завершен'),
+(1, 150, '2024-03-15', 'завершен'),
+(1, 150, '2024-05-15', 'завершен'),
+(1, 150, '2024-06-15', 'завершен'),
+(1, 150, '2024-08-15', 'завершен'),
+(1, 150, '2024-09-15', 'завершен'),
+(1, 150, '2024-12-15', 'завершен'),
+(1, 150, '2024-11-15', 'завершен'),
 (2, 17, '2024-02-10', 'в процессе'),
 (3, 130, '2024-03-05', 'завершен'),
 (1, 200, '2024-04-20', 'в процессе'),
@@ -156,26 +164,47 @@ CROSS JOIN avg_order
 WHERE o.amount > avg_order.overall_avg;
 
 -- 10
+with all_clients as(
+select
+	c.id_client,
+	c.name,
+	extract(month
+from
+	o.order_date) as order_month,
+	COUNT(o.id_order) as orders_count
+from
+	Clients c
+left join 
+    Orders o
+on
+	c.id_client = o.id_users
+where
+	extract(year
+from
+	o.order_date) = extract(year
+from
+	CURRENT_DATE)
+group by
+	c.id_client,
+	c.name,
+	extract(month
+from
+	o.order_date))
+select
+	*
+from
+	all_clients
+where
+	id_client in (
+	select
+		id_client
+	from
+		all_clients
+	group by
+		id_client
+	having
+		count(distinct order_month) = 12);
 
-WITH monthly_orders AS (
-    SELECT 
-        c.id_client,
-        c.name,
-        TO_CHAR(o.order_date, 'YYYY-MM') AS order_month,
-        COUNT(*) AS order_count
-    FROM Clients c
-    JOIN Orders o ON c.id_client = o.id_users
-    WHERE EXTRACT(YEAR FROM o.order_date) = EXTRACT(YEAR FROM CURRENT_DATE) -- текущий год
-    GROUP BY c.id_client, c.name, TO_CHAR(o.order_date, 'YYYY-MM') 
-)
-SELECT 
-    id_client,
-    name,
-    COUNT(DISTINCT order_month) AS months_with_orders,
-    SUM(order_count) AS total_orders_this_year
-FROM monthly_orders
-GROUP BY id_client, name
-HAVING COUNT(DISTINCT order_month) = 12;
 
 -- 11
 
@@ -218,7 +247,7 @@ BEGIN
             COUNT(DISTINCT c.id_client), COALESCE(SUM(o.amount), 0)
         INTO total_clients, total_order_amount
         FROM Clients c
-        LEFT JOIN Orders o ON c.id_client = o.id_client
+        LEFT JOIN Orders o ON c.id_client = o.id_users
         WHERE c.age < 30;
 
     ELSIF age_group = 'от 30 до 50' THEN
@@ -226,7 +255,7 @@ BEGIN
             COUNT(DISTINCT c.id_client), COALESCE(SUM(o.amount), 0)
         INTO total_clients, total_order_amount
         FROM Clients c
-        LEFT JOIN Orders o ON c.id_client = o.id_client
+        LEFT JOIN Orders o ON c.id_client = o.id_users
         WHERE c.age BETWEEN 30 AND 50;
 
     ELSIF age_group = 'старше 50' THEN
@@ -234,7 +263,7 @@ BEGIN
             COUNT(DISTINCT c.id_client), COALESCE(SUM(o.amount), 0)
         INTO total_clients, total_order_amount
         FROM Clients c
-        LEFT JOIN Orders o ON c.id_client = o.id_client
+        LEFT JOIN Orders o ON c.id_client = o.id_users
         WHERE c.age > 50;
 
     ELSE
@@ -243,10 +272,25 @@ BEGIN
 END;
 $$;
 
-call get_clients_and_order_sum_by_age_group('от 30 до 50');
+DO $$ 
+DECLARE 
+    v_total_clients INT;
+    v_total_order_amount NUMERIC;
+BEGIN
+    CALL get_clients_and_order_sum_by_age_group('старше 50', v_total_clients, v_total_order_amount);
+    RAISE NOTICE 'Total clients: %', v_total_clients;
+    RAISE NOTICE 'Total order amount: %', v_total_order_amount;
+END $$;
 
 
 -- 13
+
+select c.name, c.age, o.amount 
+FROM Clients c
+join Orders o
+on o.id_users = c.id_client
+where c.age > 50;
+
 
 CREATE OR REPLACE PROCEDURE apply_discount_for_senior_clients()
 LANGUAGE plpgsql AS $$
@@ -255,12 +299,20 @@ BEGIN
     UPDATE Orders o
     SET amount = amount * 0.9
     FROM Clients c
-    WHERE o.id_client = c.id_client
+    WHERE o.id_users = c.id_client
       AND c.age > 50;
 
     RAISE NOTICE 'Скидка 10%% применена для клиентов старше 50 лет.';
 END;
 $$;
+
+call apply_discount_for_senior_clients();
+
+select c.name, c.age, o.amount 
+FROM Clients c
+join Orders o
+on o.id_users = c.id_client
+where c.age > 50;
 
 -- 14
 
@@ -273,33 +325,29 @@ DECLARE
     from_exists BOOLEAN;
     to_exists BOOLEAN;
 BEGIN
-    -- Проверяем существование клиентов
+
     SELECT EXISTS(SELECT 1 FROM Clients WHERE id_client = from_id_client) INTO from_exists;
     SELECT EXISTS(SELECT 1 FROM Clients WHERE id_client = to_id_client) INTO to_exists;
 
-    -- Если хотя бы один из клиентов не существует, вызываем исключение
+
     IF NOT from_exists THEN
         RAISE EXCEPTION 'Клиент с ID % не существует.', from_id_client;
     ELSIF NOT to_exists THEN
         RAISE EXCEPTION 'Клиент с ID % не существует.', to_id_client;
     END IF;
 
-    -- Начинаем транзакцию
-    BEGIN
-        UPDATE Orders
-        SET id_client = to_id_client
-        WHERE id_client = from_id_client;
+    UPDATE Orders
+    SET id_users = to_id_client
+    WHERE id_users = from_id_client;
 
-        -- Проверка: если не было ошибок, фиксируем транзакцию
-        COMMIT;
+    RAISE NOTICE 'Заказы клиента % успешно перенесены к клиенту %.', from_id_client, to_id_client;
 
-        RAISE NOTICE 'Заказы клиента % успешно перенесены к клиенту %.', from_id_client, to_id_client;
+EXCEPTION
+    WHEN OTHERS THEN
 
-    EXCEPTION
-        WHEN OTHERS THEN
-            -- В случае ошибки откатываем транзакцию
-            ROLLBACK;
-            RAISE EXCEPTION 'Ошибка при переносе заказов: %', SQLERRM;
-    END;
+        RAISE EXCEPTION 'Ошибка при переносе заказов: %', SQLERRM;
 END;
 $$;
+
+
+CALL transfer_orders(1, 4);
--- 4thgrade/db/lab/lab5/lab5.sql
+++ 4thgrade/db/lab/lab5/lab5.sql
@@ -7,19 +7,21 @@ create table products (
 );
 
 create table orders (
-    id INT  primary key generated always as identity,
-    amount int not null 
+    id INT primary key generated always as identity,
+    amount int not null,
     creation_date date not null,
-    status varchar(50) CHECK (status IN ('завершен', 'отменен', 'в обработке')),
-    product_id INT NOT NULL,  
-    FOREIGN KEY (product_id) REFERENCES Products(product_id) ON DELETE CASCADE
+    status varchar(50) check (status in ('завершен', 'отменен', 'в обработке')),
+    product_id INT not null,  
+    foreign key (product_id) references Products(id) on
+delete
+	cascade
 );
 
 
 CREATE INDEX idx_category ON Products (category);
-CREATE INDEX idx_order_date ON Orders (order_date);
+CREATE INDEX idx_order_date ON Orders (creation_date);
 
-INSERT INTO Products (name, category, price, added_date)
+INSERT INTO Products (name, category, price, creation_date)
 VALUES 
     ('Product A', 'Electronics', 100.00, '2022-06-01'),
     ('Product B', 'Electronics', 101.00, '2022-06-02'), 
@@ -31,11 +33,11 @@ VALUES
     ('Product EE', 'Food', 26.00, '2023-01-05'),
     ('Product F', 'Books', 50.00, '2022-06-03'),
     ('Product G', 'Clothing', 500.00, '2022-09-10'),
-    ('Product H', 'Food', 75.00, '2021-06-04');
+    ('Product H', 'Food', 75.00, '2021-06-04'),
+   ('Product HH', 'Food', 144000, '2021-06-04');
     
 
-
-INSERT INTO Orders (product_id, quantity, order_date, status)
+INSERT INTO Orders (product_id, amount, creation_date, status)
 VALUES 
     (8, 52, '2024-05-12', 'в обработке'), 
     (8, 52, '2023-05-12', 'завершен'), 
@@ -68,53 +70,348 @@ ORDER BY p.price DESC;
 
 -- 2
 
-select p.id, p.category, p.name, p.price, p.creation_date
-from products p
-join 
-(SELECT category, 
-        first_value(id) OVER (partition by category order by price) as min_id, 
-        last_value(id) OVER (partition by category order by price) as max_id
-FROM products) pp
-where p.id = pp.min_id or p.id = p.max_id
-order by p.category, p.price;
+--with pp as (
+select p.id,p.category, p.price, p.name
+from products p 
+join (select category, min(price), max(price)
+from products p 
+group by category) pp 
+on pp.category = p.category and (p.price = pp.min or p.price = pp.max)
+order by category, price;
 
 -- 3 
 
-SELECT p.id, p.name, p.category, p.price, p.added_date
+SELECT p.id, p.name, p.category, p.price, p.creation_date 
 FROM Products p
 LEFT JOIN Orders o ON p.id = o.product_id
 WHERE o.product_id IS NULL
-ORDER BY p.added_date DESC;
+ORDER BY p.creation_date DESC;
 
 -- 4
 
-SELECT p.id, p.name, p.category, p.price
+WITH AvgOrderPrice AS (
+    SELECT AVG(o.amount*p.price) AS avg_price
+    FROM Orders o
+    join products p on o.product_id = p.id 
+    WHERE o.status = 'завершен'
+      AND EXTRACT(YEAR FROM o.creation_date) = 2023
+)
+SELECT p.id, p.name, p.category, p.price, AvgOrderPrice.avg_price
 FROM Products p
-JOIN Orders o ON p.id = o.product_id
-WHERE o.status = 'завершен' AND EXTRACT(YEAR FROM o.order_date) = 2023
-GROUP BY p.id, p.name, p.category, p.price
-HAVING p.price > AVG(o.quantity * p.price);
+cross JOIN AvgOrderPrice 
+WHERE p.price > AvgOrderPrice.avg_price;
 
 -- 5
 
-SELECT p.category, p.id, p.name, SUM(o.quantity) AS total_quantity
-FROM Products p
-JOIN Orders o ON p.id = o.product_id
-WHERE o.status = 'завершен'
-GROUP BY p.category, p.id, p.name
-HAVING SUM(o.quantity) > 50
-ORDER BY p.category, total_quantity DESC;
+SELECT 
+    p.id AS product_id,
+    p.name AS product_name,
+    p.category,
+    COUNT(o.id) AS completed_orders_count
+FROM 
+    Products p
+JOIN 
+    Orders o
+ON 
+    p.id = o.product_id
+WHERE 
+    o.status = 'завершен'
+GROUP BY 
+    p.id, p.name, p.category
+HAVING 
+    COUNT(o.id) > 50
+ORDER BY 
+    p.category, completed_orders_count DESC;
 
 -- 6
 
-select p.id, p.name, p.category
-from products
-join (select * from orders where amount > 10) on p.id = o.product_id;
+select
+	p.id,
+	p.name,
+	p.category
+from
+	products p
+join (
+	select
+		*
+	from
+		orders
+	where
+		amount > 10) o on
+	p.id = o.product_id;
 
 -- 7
+-- Выведите продукты, которые были заказаны в каждом месяце 2023 года, с
+-- указанием количества заказов в каждом месяце.
 
-
-
+with all_Products as(
+select
+	c.id,
+	c.name,
+	extract(month
+from
+	o.creation_date) as order_month,
+	COUNT(o.id) as orders_count
+from
+	Products c
+left join 
+    Orders o
+on
+	c.id = o.product_id
+where
+	extract(year
+from
+	o.creation_date) = 2023
+group by
+	c.id,
+	c.name,
+	extract(month
+from
+	o.creation_date))
+select
+	*
+from
+	all_Products
+where
+	id in (
+	select
+		id
+	from
+		all_Products
+	group by
+		id
+	having
+		count(distinct order_month) = 12);
+	
 -- 8
-
+	
+WITH AvgPrice AS (
+    SELECT AVG(price) AS avg_price
+    FROM Products
+),
+NeverCancelled AS (
+    SELECT 
+        p.id
+    FROM 
+        Products p
+    LEFT JOIN 
+        Orders o
+    ON 
+        p.id = o.product_id AND o.status = 'отменен'
+    WHERE 
+        o.id IS NULL
+)
+SELECT 
+    p.id, 
+    p.name, 
+    p.category, 
+    p.price
+FROM 
+    Products p
+JOIN 
+    AvgPrice ap
+ON 
+    p.price > ap.avg_price
+JOIN 
+    NeverCancelled nc
+ON 
+    p.id = nc.id;
+   
 -- 9
+   
+WITH StatusCounts AS (
+    SELECT 
+        p.id AS product_id, 
+        p.name AS product_name, 
+        o.status,
+        COUNT(o.id) AS status_count,
+        MIN(o.creation_date) AS first_order_date,
+        MAX(o.creation_date) AS last_order_date
+    FROM 
+        Products p
+    JOIN 
+        Orders o
+    ON 
+        p.id = o.product_id
+    WHERE 
+        o.status IN ('завершен', 'отменен')
+    GROUP BY 
+        p.id, p.name, o.status
+)
+SELECT 
+    sc.product_id, 
+    sc.product_name, 
+    MIN(sc.first_order_date) AS first_order_date, 
+    MAX(sc.last_order_date) AS last_order_date
+FROM 
+    StatusCounts sc
+GROUP BY 
+    sc.product_id, sc.product_name
+HAVING 
+    COUNT(DISTINCT sc.status) = 2;   
+   
+-- 10
+   
+WITH MonthlyProductOrders AS (
+    SELECT 
+        p.id AS product_id, 
+        p.name AS product_name, 
+        EXTRACT(YEAR FROM o.creation_date) AS order_year,
+        EXTRACT(MONTH FROM o.creation_date) AS order_month,
+        SUM(o.amount) AS total_amount
+    FROM 
+        Products p
+    JOIN 
+        Orders o
+    ON 
+        p.id = o.product_id
+    GROUP BY 
+        p.id, p.name, EXTRACT(YEAR FROM o.creation_date), EXTRACT(MONTH FROM o.creation_date)
+),
+MaxOrders AS (
+    SELECT 
+        order_year, 
+        order_month, 
+        MAX(total_amount) AS max_amount
+    FROM 
+        MonthlyProductOrders
+    GROUP BY 
+        order_year, order_month
+)
+SELECT 
+    mpo.product_id, 
+    mpo.product_name, 
+    mpo.order_year, 
+    mpo.order_month, 
+    mpo.total_amount
+FROM 
+    MonthlyProductOrders mpo
+JOIN 
+    MaxOrders mo
+ON 
+    mpo.order_year = mo.order_year AND 
+    mpo.order_month = mo.order_month AND 
+    mpo.total_amount = mo.max_amount
+order by order_year, order_month;
+
+-- 11
+
+CREATE OR REPLACE FUNCTION check_order_amount()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.amount < 1 THEN
+        RAISE EXCEPTION 'Сумма заказа не может быть менее 0';
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER check_no_less_than_one
+BEFORE INSERT OR UPDATE ON Orders 
+FOR EACH ROW
+EXECUTE FUNCTION check_order_amount();
+
+INSERT INTO Orders (product_id, amount, creation_date, status)
+VALUES 
+(1, 0, '2023-01-15', 'завершен');
+
+-- 12
+
+CREATE OR REPLACE FUNCTION log_price_changes()
+RETURNS TRIGGER AS $$
+BEGIN
+	CREATE TABLE IF NOT EXISTS PriceLogs (
+	    id SERIAL PRIMARY KEY,
+	    product_id INT NOT NULL,
+	    old_price DECIMAL(10, 2),
+	    new_price DECIMAL(10, 2),
+	    change_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	);
+    INSERT INTO PriceLogs (product_id, old_price, new_price)
+    VALUES (NEW.id, OLD.price, NEW.price);
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER price_update_trigger
+AFTER UPDATE OF price ON Products
+FOR EACH ROW
+WHEN (OLD.price IS DISTINCT FROM NEW.price)
+EXECUTE FUNCTION log_price_changes();
+
+UPDATE Products
+SET price = price * 1.10
+WHERE id = 1;
+
+UPDATE Products
+SET price = price * 1.15
+WHERE id = 2;
+
+UPDATE Products
+SET price = price * 0.90
+WHERE id = 3;
+
+select * from PriceLogs;
+
+-- 13
+CREATE OR REPLACE PROCEDURE get_total_orders_by_category(
+    IN category_name VARCHAR(100),
+    OUT total_units INT
+)
+LANGUAGE plpgsql AS $$
+BEGIN
+    SELECT SUM(o.amount)
+    INTO total_units
+    FROM Orders o
+    JOIN Products p ON o.product_id = p.id
+    WHERE p.category = category_name;
+END;
+$$;
+
+DO $$ 
+DECLARE 
+    total_units INT;
+BEGIN
+    call get_total_orders_by_category('Food', total_units);
+    RAISE NOTICE 'Total units: %', total_units;
+END $$;
+
+-- 14
+
+CREATE OR REPLACE PROCEDURE cancel_orders_by_product_limit(
+    IN product_id INT,
+    IN order_limit INT
+)
+LANGUAGE plpgsql AS $$
+BEGIN
+    UPDATE Orders
+    SET status = 'отменен'
+    WHERE product_id = product_id
+      AND (SELECT COUNT(*) FROM Orders WHERE product_id = product_id) > order_limit;
+END;
+$$;
+
+-- 15
+
+CREATE OR REPLACE PROCEDURE increase_price_for_large_orders()
+LANGUAGE plpgsql AS $$
+DECLARE
+    order_cursor CURSOR FOR 
+        SELECT p.id, p.price
+        FROM Products p
+        JOIN Orders o ON p.id = o.product_id
+        GROUP BY p.id, p.price
+        HAVING SUM(o.amount) > 50;
+    product_record RECORD;
+BEGIN
+    OPEN order_cursor;
+    LOOP
+        FETCH order_cursor INTO product_record;
+        EXIT WHEN NOT FOUND;
+        UPDATE Products
+        SET price = price * 1.05
+        WHERE id = product_record.id;
+    END LOOP;
+    CLOSE order_cursor;
+END;
+$$;
